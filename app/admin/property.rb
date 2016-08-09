a8052082244382593e75052fd12ed8ae0271ac9c
Prelang Builder <builder@prelang.com>
2016-08-09 13:37:28 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Property' to allow for create, update

--- app/admin/property.rb
+++ app/admin/property.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Property do
+  permit_params :property_type, :property_sale_or_rental, :property_price_per_month, :property_price_for_sale, :property_rental_deposit, :property_first_line_address, :property_postcode, :property_region, :property_listing_type, :property_number_of_bedrooms, :property_number_of_bathrooms, :property_size_in_metres, :property_condition, :property_near_banks, :property_is_new_build, :property_has_elevator, :property_has_garage, :property_has_private_pool, :property_has_communal_pool, :property_has_terrace, :property_has_photos, :property_has_airconditioning, :property_has_garden, :property_has_storage, :property_has_wardrobes, :property_floor, :property_photos
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
